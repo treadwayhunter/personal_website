238081267f1ae895347825ba4b8d63536bde9ab8
Hunter Treadway <treadwayhunter@gmail.com>
2025-10-18 22:38:28 +0100

    Init commit.

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import '../styles/Main.css'
+import Header from "../components/Header"
+
+const Main = () => {
+    return (
+        <div className="main">
+            <Header />
+        </div>
+    );
+}
+export default Main;
