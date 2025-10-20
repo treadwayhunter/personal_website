372e90177efb8894e26ef35cae05a1fbccdf64a6
Hunter Treadway <treadwayhunter@gmail.com>
2025-10-20 09:53:26 +0100

    Began working navigation using a browser router.
    I might keep the browser router, but I might change the tab navigation to work the way I'd like.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,9 @@
-import '../styles/Main.css'
-import Header from "../components/Header"
-
 const Main = () => {
     return (
         <div className="main">
-            <Header />
+
         </div>
     );
 }
+
 export default Main;
