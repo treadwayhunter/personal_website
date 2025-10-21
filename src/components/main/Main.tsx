e6347f04bd15fe728d2c5246ab3fc77acf07159e
Hunter Treadway <treadwayhunter@gmail.com>
2025-10-21 11:23:07 +0100

    Set the basis to the footer. Can come back and adjust when necessary.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,12 +1,14 @@
 import '../../styles/Main.css'
 import Header from "./Header"
 import TabScreen from "../tabScreens/TabScreen";
+import Footer from "./Footer";
 
 const Main = () => {
     return (
         <div className="main">
             <Header />
             <TabScreen />
+            <Footer />
         </div>
     );
 }
