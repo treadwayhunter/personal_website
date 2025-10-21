92798bf23f5ebff44e96b20584464cf9492c3b71
Hunter Treadway <treadwayhunter@gmail.com>
2025-10-21 13:03:41 +0100

    Got hit by a flex gotcha. But finally managed to get a single image working without resizing the whole page.

--- src/components/tabScreens/MyselfScreen.tsx
+++ src/components/tabScreens/MyselfScreen.tsx
@@ -1,8 +1,21 @@
+import image1 from '../../assets/images/graduate001.jpg';
 import '../../styles/MyselfScreen.css';
 
 const MyselfScreen = () => {
     return (
-      <div className='myself-screen'>HELLO WORLD</div>
+      <div className='myself-screen'>
+          <div className="myself-screen-left">
+              <div className="image-container">
+                  <img src={image1}/>
+              </div>
+          </div>
+          <div className="vertical-divider">
+              <div/>
+          </div>
+          <div className="myself-screen-right">
+              <span>Start working on this</span>
+          </div>
+      </div>
     );
 }
 
--- src/components/tabScreens/TabScreen.tsx
+++ src/components/tabScreens/TabScreen.tsx
@@ -5,7 +5,11 @@ import {useMainTabContext} from "../../contexts/MainTabContext";
 const TabScreen = () => {
     const {tabState} = useMainTabContext();
     const Active = tabState.find(t => t.selected)!.screen;
-    return <Active />
+    return (
+        <div className="tab-screen">
+            <Active />
+        </div>
+    );
 }
 
 export default TabScreen;
